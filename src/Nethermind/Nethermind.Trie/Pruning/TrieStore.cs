36fdc2be8dd5fb4ad34fce749e6aefe1258be0a1
MarekM25 <marekm2504@gmail.com>
2022-07-14 13:31:56 +0200

    more logs

--- src/Nethermind/Nethermind.Trie/Pruning/TrieStore.cs
+++ src/Nethermind/Nethermind.Trie/Pruning/TrieStore.cs
@@ -241,6 +241,10 @@ namespace Nethermind.Trie.Pruning
                 }
 
                 node = SaveOrReplaceInDirtyNodesCache(nodeCommitInfo, node);
+                if (blockNumber < (node.LastSeen ?? 0))
+                {
+                    if (_logger.IsWarn) _logger.Warn($"Found blockNumber {blockNumber} lower than node {node.LastSeen}, node.Keccak: {node.Keccak} ");
+                }
                 node.LastSeen = Math.Max(blockNumber, node.LastSeen ?? 0);;
 
                 if (!_pruningStrategy.PruningEnabled)
